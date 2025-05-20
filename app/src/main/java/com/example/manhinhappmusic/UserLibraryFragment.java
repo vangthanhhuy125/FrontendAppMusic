4d0499008273a6d960d9a4e75bce7b8eb07c6738
vangthanhhuy125 <vangthanhhuy010205@gmail.com>
2025-05-20 23:34:56 +0700

    UserLibraryFragment

--- app/src/main/java/com/example/manhinhappmusic/UserLibraryFragment.java
+++ app/src/main/java/com/example/manhinhappmusic/UserLibraryFragment.java
@@ -1,5 +1,6 @@
 package com.example.manhinhappmusic;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -7,14 +8,20 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.material.imageview.ShapeableImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -47,6 +54,9 @@ public class UserLibraryFragment extends BaseFragment {
     // TODO: Rename and change types and number of parameters
     private ShapeableImageView userAvatar;
     private ImageButton addButton;
+    private RecyclerView songView;
+    private LibraryAdapter songAdapter;
+    private List<Playlist> songItems;
     public static UserLibraryFragment newInstance(String param1, String param2) {
         UserLibraryFragment fragment = new UserLibraryFragment();
         Bundle args = new Bundle();
@@ -79,6 +89,33 @@ public class UserLibraryFragment extends BaseFragment {
         userAvatar.setOnClickListener(this::onUserAvatarClick);
         addButton = view.findViewById(R.id.add_playlist_button);
         addButton.setOnClickListener(this::onAddPlaylistButtonClick);
+
+        List<String> songs1 = new ArrayList<>();
+        songs1.add("ABC");
+        songs1.add("DEF");
+        songs1.add("EEEE");
+        songs1.add("123456789");
+        songs1.add("HHH");
+        List<String> songs2 = new ArrayList<>();
+        songs2.add("XXXXX");
+        songs2.add("ZZZZ");
+
+        songItems = new ArrayList<>();
+        songItems.add(new Playlist("1","Cai luong", "xxx", songs1, "1", "url"));
+        songItems.add(new Playlist("2","Tan co", "xxx", songs2, "1", "url"));
+        songItems.add(new Playlist("3","Son Tung MTP", "xxx", songs2, "1", "url"));
+        songItems.add(new Playlist("4","Nhac Remix", "xxx", songs1, "1", "url"));
+        songItems.add(new Playlist("5","Nhac Thieu Nhi", "xxx", songs1, "1", "url"));
+
+        songAdapter = new LibraryAdapter(songItems);
+        songView = view.findViewById(R.id.playlist_list);
+        songView.setAdapter(songAdapter);
+        Context context = getContext();
+        if (context != null) {
+            LinearLayoutManager layoutManager = new LinearLayoutManager(context);
+            songView.setLayoutManager(layoutManager);
+        }
+
 
     }
 
