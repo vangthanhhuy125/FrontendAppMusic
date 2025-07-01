35c38d437888a17bf756111506da5b6477a10609
vangthanhhuy125 <165678055+vangthanhhuy125@users.noreply.github.com>
2025-07-01 16:17:15 +0700

    Update AdminHomeFragment.java

--- app/src/main/java/com/example/manhinhappmusic/AdminHomeFragment.java
+++ app/src/main/java/com/example/manhinhappmusic/AdminHomeFragment.java
@@ -1,64 +1,114 @@
 package com.example.manhinhappmusic;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdminHomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AdminHomeFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import com.bumptech.glide.Glide;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+import java.util.Arrays;
+import java.util.List;
 
-    public AdminHomeFragment() {
-        // Required empty public constructor
-    }
+public class AdminHomeFragment extends BaseFragment {
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminHomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AdminHomeFragment newInstance(String param1, String param2) {
-        AdminHomeFragment fragment = new AdminHomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private TextView totalUsers, totalArtists, totalGenres, totalSongs;
+    private RecyclerView recyclerSongRequests, recyclerArtistRequests;
+    private ImageView imageAvatar;
+
+    public static AdminHomeFragment newInstance() {
+        return new AdminHomeFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_admin_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        totalUsers = view.findViewById(R.id.textad_total_users);
+        totalArtists = view.findViewById(R.id.textad_total_artists);
+        totalGenres = view.findViewById(R.id.textad_total_genres);
+        totalSongs = view.findViewById(R.id.textad_total_songs);
+        recyclerSongRequests = view.findViewById(R.id.recycler_song_requests);
+        recyclerArtistRequests = view.findViewById(R.id.recycler_artist_requests);
+        imageAvatar = view.findViewById(R.id.image_adavatar);
+
+
+        totalUsers.setText("156");
+        totalArtists.setText("42");
+        totalGenres.setText("18");
+        totalSongs.setText("432");
+
+
+        Glide.with(this)
+                .load(R.drawable.exampleavatar)
+                .into(imageAvatar);
+
+
+        imageAvatar.setOnClickListener(v -> {
+            if (callback != null) {
+                callback.onRequestChangeFragment(FragmentTag.ADMIN_PROFILE);
+            }
+        });
+
+
+        List<String> songRequests = Arrays.asList("Song A - User1", "Song B - User2", "Song C - User3");
+        List<String> artistRequests = Arrays.asList("Artist X - User4", "Artist Y - User5");
+
+
+        recyclerSongRequests.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerSongRequests.setAdapter(new SimpleTextAdapter(songRequests));
+
+        recyclerArtistRequests.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerArtistRequests.setAdapter(new SimpleTextAdapter(artistRequests));
+    }
+
+
+    public static class SimpleTextAdapter extends RecyclerView.Adapter<SimpleTextAdapter.ViewHolder> {
+
+        private final List<String> items;
+
+        public SimpleTextAdapter(List<String> items) {
+            this.items = items;
+        }
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            TextView text;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                text = itemView.findViewById(android.R.id.text1);
+            }
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.text.setText(items.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
     }
 }
