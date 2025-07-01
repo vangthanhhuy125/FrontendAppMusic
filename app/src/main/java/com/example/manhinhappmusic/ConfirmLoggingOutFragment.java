f2e75984520fe7d0ccd2bf2686ed5341f32d13d6
vangthanhhuy125 <165678055+vangthanhhuy125@users.noreply.github.com>
2025-07-01 22:30:10 +0700

    Update ConfirmLoggingOutFragment.java

--- app/src/main/java/com/example/manhinhappmusic/ConfirmLoggingOutFragment.java
+++ app/src/main/java/com/example/manhinhappmusic/ConfirmLoggingOutFragment.java
@@ -1,65 +1,60 @@
 package com.example.manhinhappmusic;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ConfirmLoggingOutFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ConfirmLoggingOutFragment extends DialogFragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class ConfirmLoggingOutFragment extends Fragment {
 
     public ConfirmLoggingOutFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConfirmLoggingOutFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ConfirmLoggingOutFragment newInstance(String param1, String param2) {
-        ConfirmLoggingOutFragment fragment = new ConfirmLoggingOutFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static ConfirmLoggingOutFragment newInstance() {
+        return new ConfirmLoggingOutFragment();
     }
 
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_confirm_logging_out, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button cancelButton = view.findViewById(R.id.cancel_button);
+        Button logoutButton = view.findViewById(R.id.create_button);
+
+        cancelButton.setOnClickListener(v -> closeThisFragment());
+
+        logoutButton.setOnClickListener(v -> {
+            closeThisFragment();
+
+            if (requireActivity() instanceof BaseFragment.FragmentInteractionListener) {
+                ((BaseFragment.FragmentInteractionListener) requireActivity())
+                        .onRequestChangeFragment(BaseFragment.FragmentTag.LOGIN);
+            }
+        });
+    }
+
+    private void closeThisFragment() {
+        View containerView = requireActivity().findViewById(R.id.confirm_log_out_container);
+        if (containerView != null) {
+            containerView.setVisibility(View.GONE);
+        }
+
+        requireActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .remove(this)
+                .commit();
     }
 }
