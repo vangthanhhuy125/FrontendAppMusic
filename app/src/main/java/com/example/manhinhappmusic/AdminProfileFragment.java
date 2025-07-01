67e451d1d4756994251f4ec7573e1e1db4f99c03
vangthanhhuy125 <165678055+vangthanhhuy125@users.noreply.github.com>
2025-07-01 16:24:51 +0700

    Update AdminProfileFragment.java

--- app/src/main/java/com/example/manhinhappmusic/AdminProfileFragment.java
+++ app/src/main/java/com/example/manhinhappmusic/AdminProfileFragment.java
@@ -1,64 +1,80 @@
 package com.example.manhinhappmusic;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdminProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AdminProfileFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import com.bumptech.glide.Glide;
+import com.google.android.material.imageview.ShapeableImageView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class AdminProfileFragment extends BaseFragment {
 
-    public AdminProfileFragment() {
-        // Required empty public constructor
-    }
+    private ShapeableImageView avatarImage;
+    private TextView nameText, emailText, roleText, joinedText;
+    private ImageButton backButton;
+    private Button editButton, logoutButton;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AdminProfileFragment newInstance(String param1, String param2) {
-        AdminProfileFragment fragment = new AdminProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static AdminProfileFragment newInstance() {
+        return new AdminProfileFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_admin_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        avatarImage = view.findViewById(R.id.avataradmin);
+        nameText = view.findViewById(R.id.txtnameadmin);
+        emailText = view.findViewById(R.id.tvEmailAdmin);
+        roleText = view.findViewById(R.id.tvRoleAmin);
+        joinedText = view.findViewById(R.id.tvJoinedAdmin);
+        backButton = view.findViewById(R.id.btnbackadmin);
+        editButton = view.findViewById(R.id.btneditadmin);
+        logoutButton = view.findViewById(R.id.btnlogoutadmin);
+
+
+        Glide.with(this)
+                .load(R.drawable.avatar_app_music)
+                .into(avatarImage);
+
+        nameText.setText("Admin Name");
+        emailText.setText("admin@example.com");
+        roleText.setText("Admin");
+        joinedText.setText("22/12/2024");
+
+
+        backButton.setOnClickListener(v -> {
+            if (callback != null) {
+                callback.onRequestChangeFragment(FragmentTag.ADMIN_HOME);
+            }
+        });
+
+
+
+        editButton.setOnClickListener(v -> {
+            if (callback != null) {
+                callback.onRequestChangeFragment(FragmentTag.EDIT_PROFILE);
+            }
+        });
+
+
+        logoutButton.setOnClickListener(v -> {
+            if (callback != null) {
+                callback.onRequestOpenBottomSheetFragment(FragmentTag.CONFIRM_LOGGING_OUT);
+            }
+        });
     }
 }
